a08d51833005fa622e665c95bde9d5c5f17790a5
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-20 11:54:39 +0530

    Added:-Using case statment

--- emps.sh
+++ emps.sh
@@ -1,32 +1,30 @@
-#!/bin/bash -x  
+#!/bin/bash   
 echo "Welcome to employee wage computation"
 function check_attend()
 {
   attend=$((RANDOM%3))
-  if [ $attend -eq 0 ]
-  then 
-       echo 0
-  elif [ $attend  -eq 1 ]
-  then 
-       echo 1
-  else
-       echo 2
-  fi
+  case $attend in
+  0) 
+       echo "0" ;;
+  1) 
+       echo "1" ;;
+  2)
+       echo "2" ;;
+  esac
 }
 res="$( check_attend $(()) )"
 
 function check_Employee_wage()
 {
-  if [ $res -eq 0 ]
-  then
+    case $res in
+    0)
          emp_Daily_wages=$((20*8))
-         echo $emp_Daily_wages
-  elif [ $res -eq 1 ]
-  then
+         echo $emp_Daily_wages ;;
+    1)
          partime_Employee_wage=$((20*4))
-         echo $partime_Employee_wages
-  else
-         echo "employee is absent"
-  fi
+         echo $partime_Employee_wage ;;
+    2)
+         echo "employee is absent" ;;
+    esac
 }
 check_Employee_wage
