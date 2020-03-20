88b79fab270b733007a499635d0245940fb64e95
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-20 10:44:32 +0530

    Added:-employee attendence

--- emps.sh
+++ emps.sh
@@ -1,2 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash -x  
 echo "Welcome to employee wage computation"
+function check_attend()
+{
+  attend=$((RANDOM%2))
+  if [ $attend -eq 0 ]
+  then 
+       echo 0
+  else 
+       echo 1
+  fi
+}
+check_attend
