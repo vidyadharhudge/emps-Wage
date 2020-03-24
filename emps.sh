316669f354322e82533cdf434fa7d99ace3cac53
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-24 14:47:26 +0530

    Added:-Store Day And Daily Wages Along With Total Wages

--- emps.sh
+++ emps.sh
@@ -1,66 +1,68 @@
 #!/bin/bash 
 declare -A storeing_Wages 
 echo "Welcome to employee wage computation"
-function check_attend()
+function Check_Attend()
 {
-  attend=$((RANDOM%2))
-  case $attend in
-  0) 
-       echo "0" ;;
-  1) 
-       echo "1" ;;
+     attend=$((RANDOM%2))
+     case $attend in
+     0) 
+         echo "0" ;;
+     1) 
+         echo "1" ;;
  
-  esac
+     esac
 }
-function gethour(){
-val="$( check_attend $(()) )"
-if [ $val -eq 0 ]
-then
-	val1=$((RANDOM%2))
-	case $val1 in 
-	0)
-		hours=8
-	echo $hours ;;
-   1)	
-		hours=4
-		echo $hours ;;
-esac
-fi
+
+function gethour()
+{
+    val="$( Check_Attend $(()) )"
+    if [ $val -eq 0 ]
+    then
+  	 val1=$((RANDOM%2))
+	 case $val1 in 
+  	 0)
+	   	hours=8
+	      echo $hours ;;
+    1)	
+	   	hours=4
+		   echo $hours ;;
+    esac
+    fi
 }
 
 
-function check_Employee_wage()
+function Check_Employee_Wage()
 {
 
-         val=$( gethour $(()) )
-         emp_Daily_wages=$((20*val))
-         echo $emp_Daily_wages
+    val=$( gethour $(()) )
+    Emp_Daily_Wages=$((20*val))
+    echo $Emp_Daily_Wages
 
 }
 
-function cal_wage_for_month()
+function Calculate_Wage_For_Month()
 {
-    count=0
-    hour=0
-    calculatinghour=0
-    total_wage=1
+     count=0
+     hour=0
+     calculatinghour=0
+     total_wage=1
   while [[ $count -le 20 ]] && [[ $hour -le 100 ]]
   do
        hour=$( gethour $(()) )
        calculatinghour=$(( $calculatinghour + hour ))
-       res=$( check_Employee_wage $(()) )
+       res=$( Check_Employee_Wage $(()) )
        total_wage=$(( $total_wage + res ))
        storeing_Wages[$count]=$res
        ((count++))
-  if [ $count -eq 20 ]
-  then
-       break;
-  fi
-  done
-       echo "total wages are:$total_wage"
-        echo "total days are:${!storeing_Wages[@]}"
-       echo "daily wages are:${storeing_Wages[@]}"
+       if [ $count -eq 20 ]
+       then
+            break;
+       fi
+       done
+             echo "total wages are:$total_wage"
+             echo "total days are:${!storeing_Wages[@]}"
+             echo "daily wages are:${storeing_Wages[@]}"
 
  }
-cal_wage_for_month
+Calculate_Wage_For_Month
 
